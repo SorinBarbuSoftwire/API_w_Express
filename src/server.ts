789f2abef73a91917e1701583c32f9d9654149ab
Sorin Barbu <Sorin.Barbu@sofwire.com>
2023-06-30 12:50:05 +0300

    done rovers from file

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,34 @@
-import {Request, Response} from "express";
-
+import {Request, response, Response} from "express";
 const express = require("express");
+import * as fs from 'fs';
+const axios = require("axios");
+
+const roverData:string = fs.readFileSync('./data/rovers.json', 'utf-8');
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
-const router = express.Router();
-router.get('/test', (req: Request, res : Response) => res.send('Hello world !'));
-app.use('/', router);
+const routerTest = express.Router();
+const routerRovers = express.Router();
+routerTest.get('/test', testController);
+routerRovers.get('/rovers', roverController)
+app.use('/', routerTest);
+app.use('/', routerRovers);
 
 app.listen(port, () => {
     console.log(`Test backend is running on port ${port}`);
 });
 
-import * as fs from 'fs';
+function testController(req:Request, res:Response) : void {
+    res.send('Hello world !');
+}
 
-const words = fs.readFileSync('./words.txt', 'utf-8');
-console.log(words);
+function roverController(req:Request, res:Response) : void {
+    res.send(roverData);
+    // axios.get("https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=t2DwwSTymKQ5rd5M9h0hc6xdL9lPuchtdQx37pDV")
+    //     .then((response) => {
+    //         res.send(response);
+    //     })
+
+}
